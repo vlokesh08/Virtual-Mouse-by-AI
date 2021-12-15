14c158339829cd05d140dd7f89ddf94446adc1c1
Venkata Lokesh <76467286+vlokesh08@users.noreply.github.com>
2021-12-15 10:44:04 +0530

    Add files via upload

---
+++ HandTrackingMod.py
@@ -0,0 +1,38 @@
+import cv2
+import time
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+cTime = 0
+pTime = 0
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                if (id==8):
+                    cv2.circle(img, (cx,cy), 25, (255, 0 ,0), cv2.FILLED)
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+
+    #Fps meter
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3,(255,0,0),3)
+
+
+    cv2.imshow("Video", img)
+    cv2.waitKey(1)
